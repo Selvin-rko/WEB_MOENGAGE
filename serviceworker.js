4ccaa328002ca85f7a64dc5260c204dbaecc393f
Selvin-rko <98406215+Selvin-rko@users.noreply.github.com>
2024-02-12 14:04:50 +0530

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,2 +1,14 @@
 /*! For license information please see serviceworker.js.LICENSE.txt */
 importScripts("https://cdn.moengage.com/release/dc_1/serviceworker_cdn.min.latest.js");
+
+console.log('Started', self);
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('Installed', event);
+});
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+});
+self.addEventListener('push', function(event) {
+  console.log('Push message received', event);
+});
